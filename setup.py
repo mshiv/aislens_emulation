519ed6081f267e7ac7be8b10bad130f46018dce8
Shivaprakash Muruganandham <smurugan9@gatech.edu>
2022-03-01 13:27:40 -0500

    Initialize project repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Statistical emulation of Antarctic Ice Sheet melt projections',
+    author='Shivaprakash Muruganandham',
+    license='BSD-3',
+)
