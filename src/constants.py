83e91f88acd998f1863f971bf72e9c38dd911445
Shivaprakash Muruganandham <shivamurug@gmail.com>
2023-10-31 23:48:20 -0400

    add constants module

---
+++ src/constants.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# conversion factor from radians to degrees
+rad_to_deg = 180. / np.pi
+
+# conversion factor from degrees to radians
+deg_to_rad = np.pi / 180.
+
+# seconds per day
+sec_per_day = 86400.
+
+# seconds in a year
+sec_per_year = sec_per_day * 365.
+
+# seconds per month (approximate)
+sec_per_month = sec_per_day * 30.
+
+# density of freshwater (kg/m^3)
+rho_fw = 1000.
+
+# kilograms per gigatonne
+kg_per_GT = 1e12
+
+# cm per m
+cm_per_m = 100.
